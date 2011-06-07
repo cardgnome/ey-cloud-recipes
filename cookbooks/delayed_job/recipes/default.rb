c53d7b7eac43aee9c41ddf42b964d4c09b700d66
dignoe <chad.mcgimpsey@cebacosoftware.com>
2011-06-07 16:17:41 -0600

    updated delayed_job recipe to run on app_master or solo instances

--- cookbooks/delayed_job/recipes/default.rb
+++ cookbooks/delayed_job/recipes/default.rb
@@ -3,7 +3,8 @@
 # Recipe:: default
 #
 
-if node[:instance_role] == "solo" || (node[:instance_role] == "util" && node[:name] !~ /^(mongodb|redis|memcache)/)
+# if node[:instance_role] == "solo" || (node[:instance_role] == "util" && node[:name] !~ /^(mongodb|redis|memcache)/)
+if node[:instance_role] == "solo" || node[:instance_role] == "app_master"
   node[:applications].each do |app_name,data|
   
     # determine the number of workers to run based on instance size
